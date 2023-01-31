f12d18e294a065c3b7508c1dc1fc452e95a5597b
Dolph Mathews <dolph.mathews@gmail.com>
2023-01-31 06:55:28 -0600

    add tests for resolver

---
+++ resolver_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"testing"
+)
+
+func assertLookupEquals(t *testing.T, got []string, want []string) {
+	if len(got) != len(want) {
+		t.Errorf("len(Lookup(dest)) != %d; want %d", len(got), len(want))
+	}
+	for _, wantResult := range want {
+		assertIpInResult(t, got, wantResult)
+	}
+}
+
+func assertIpInResult(t *testing.T, got []string, want string) bool {
+	for _, gotResult := range got {
+		if gotResult == want {
+			return true
+		}
+	}
+	t.Errorf("Lookup(dest) = %s; missing %s", got, want)
+	return false
+}
+
+func assertLenOfResultsInRange(t *testing.T, got []string, wantMin int, wantMax int) {
+	if wantMin > wantMax {
+		t.Errorf("wantMin must be less than wantMax")
+	} else if len(got) < wantMin {
+		t.Errorf("len(Lookup(dest)) = %s; want >= %d", got, wantMin)
+	} else if len(got) > wantMax {
+		t.Errorf("len(Lookup(dest)) = %s; want <= %d", got, wantMax)
+	}
+}
+
+func TestLookupLoopbackIp(t *testing.T) {
+	dest, err := NewDestination("http://127.0.0.1")
+	assertNoError(t, dest, err)
+	got := Lookup(dest)
+	assertLookupEquals(t, got, []string{"127.0.0.1"})
+}
+
+func TestLookupPublicIp(t *testing.T) {
+	dest, err := NewDestination("http://8.8.8.8")
+	assertNoError(t, dest, err)
+	got := Lookup(dest)
+	assertLookupEquals(t, got, []string{"8.8.8.8"})
+}
+
+func TestLookupGoogle(t *testing.T) {
+	dest, err := NewDestination("https://google.com")
+	assertNoError(t, dest, err)
+	got := Lookup(dest)
+	assertLenOfResultsInRange(t, got, 2, 20)
+}
